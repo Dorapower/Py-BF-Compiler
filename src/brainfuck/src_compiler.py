e145c3fdb28fbeb5a830138cbaecdf4a3e6beb05
Lokdora <32120829+Dorapower@users.noreply.github.com>
2023-06-23 21:21:07 +0800

    chore: reduce the amount of intermediate variables

--- src/brainfuck/src_compiler.py
+++ src/brainfuck/src_compiler.py
@@ -51,63 +51,49 @@ class Compiler:
         c_free = ir.Function(module, ir.FunctionType(ir.VoidType(), [Cell.as_pointer()]), name='free')
 
         # inc, dec, input, output, mov_left, mov_right
-        # TODO: move the load command into the function call
         llvm_inc = ir.Function(module, VoidFunc, name='inc')
         block = llvm_inc.append_basic_block(name='entry')
         with builder.goto_block(block):
-            idx = builder.load(idx_ptr)
-            arr = builder.load(arr_ptr)
-            ptr = builder.gep(arr, (idx,))
-            val = builder.load(ptr)
-            val = builder.add(val, Cell(1))
+            ptr = builder.gep(builder.load(arr_ptr), (builder.load(idx_ptr),))
+            val = builder.add(builder.load(ptr), Cell(1))
             builder.store(val, ptr)
+            builder.ret_void()
 
         llvm_dec = ir.Function(module, VoidFunc, name='dec')
         block = llvm_dec.append_basic_block(name='entry')
         with builder.goto_block(block):
-            idx = builder.load(idx_ptr)
-            arr = builder.load(arr_ptr)
-            ptr = builder.gep(arr, (idx,))
-            val = builder.load(ptr)
-            val = builder.sub(val, Cell(1))
+            ptr = builder.gep(builder.load(arr_ptr), (builder.load(idx_ptr),))
+            val = builder.sub(builder.load(ptr), Cell(1))
             builder.store(val, ptr)
             builder.ret_void()
 
         llvm_input = ir.Function(module, VoidFunc, name='input')
         block = llvm_input.append_basic_block(name='entry')
         with builder.goto_block(block):
-            val = builder.call(c_getchar, [])
-            val = builder.trunc(val, Cell)
-            idx = builder.load(idx_ptr)
-            arr = builder.load(arr_ptr)
-            ptr = builder.gep(arr, (idx,))
+            ptr = builder.gep(builder.load(arr_ptr), (builder.load(idx_ptr),))
+            val = builder.trunc(builder.call(c_getchar, []), Cell)
             builder.store(val, ptr)
             builder.ret_void()
 
         llvm_output = ir.Function(module, VoidFunc, name='output')
         block = llvm_output.append_basic_block(name='entry')
         with builder.goto_block(block):
-            idx = builder.load(idx_ptr)
-            arr = builder.load(arr_ptr)
-            ptr = builder.gep(arr, (idx,))
-            val = builder.load(ptr)
-            val = builder.zext(val, LLVMInt)
+            ptr = builder.gep(builder.load(arr_ptr), (builder.load(idx_ptr),))
+            val = builder.zext(builder.load(ptr), LLVMInt)
             builder.call(c_putchar, [val])
             builder.ret_void()
 
         llvm_mov_left = ir.Function(module, VoidFunc, name='mov_left')
         block = llvm_mov_left.append_basic_block(name='entry')
         with builder.goto_block(block):
-            idx = builder.load(idx_ptr)
-            idx = builder.sub(idx, LLVMInt(1))
+            idx = builder.sub(builder.load(idx_ptr), LLVMInt(1))
             builder.store(idx, idx_ptr)
             builder.ret_void()
 
         llvm_mov_right = ir.Function(module, VoidFunc, name='mov_right')
         block = llvm_mov_right.append_basic_block(name='entry')
         with builder.goto_block(block):
-            idx = builder.load(idx_ptr)
-            idx = builder.add(idx, LLVMInt(1))
+            idx = builder.add(builder.load(idx_ptr), LLVMInt(1))
             builder.store(idx, idx_ptr)
             builder.ret_void()
 
